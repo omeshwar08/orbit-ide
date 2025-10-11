4eece6f421c9964b8a993fafe5aa0c38ad1d495d
Omeshwar Chowdhury <63541977+code-with-omeshwar@users.noreply.github.com>
2025-10-11 19:57:23 +0530

    authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import UserButton from "@/modules/auth/components/user-button";
 
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen bg-gray-100">
 			<Button>Get Started</Button>
+			<UserButton />
 		</div>
 	);
 }
